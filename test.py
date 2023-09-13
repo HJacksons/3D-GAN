f07686bec900b918e5a4cceb02ae5ebbc298444f
HJacksons <jacksonherberts@gmail.com>
2023-09-13 19:07:25 +0200

    Add new network, dataset and classifier files

---
+++ test.py
@@ -0,0 +1,132 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import dataset
+from networks import Generator, Discriminator  # Import your Generator and Discriminator
+from torchsummary import summary
+import matplotlib.pyplot as plt
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Load the pre-trained discriminator
+discriminator = Discriminator().to(device)
+discriminator.load_state_dict(torch.load("discriminator_ckpt_1000"))  # Update the path to your pre-trained discriminator checkpoint
+
+# Define a feature extractor that extracts features from layers 2, 3, and 4 and applies max pooling
+class FeatureExtractor(nn.Module):
+    def __init__(self, discriminator):
+        super(FeatureExtractor, self).__init__()
+        self.layer1 = list(discriminator.layers.children())[0]
+        self.layer2 = list(discriminator.layers.children())[2]
+        self.layer3 = list(discriminator.layers.children())[5]
+        self.layer4 = list(discriminator.layers.children())[8]
+        self.maxpool2 = nn.MaxPool3d(kernel_size=8)
+        self.maxpool3 = nn.MaxPool3d(kernel_size=4)
+        self.maxpool4 = nn.MaxPool3d(kernel_size=2)
+
+    def forward(self, x):
+        # Pass input through layers 1 to 4
+        out1 = self.layer1(x)
+        out2 = self.layer2(out1)
+        out3 = self.layer3(out2)
+        out4 = self.layer4(out3)
+        
+        # Apply max pooling separately to layers 2, 3, and 4
+        pooled2 = self.maxpool2(out2)
+        pooled3 = self.maxpool3(out3)
+        pooled4 = self.maxpool4(out4)
+        
+        # Concatenate the pooled results
+        concatenated_features = torch.cat((pooled2, pooled3, pooled4), dim=1)
+        # print(concatenated_features.shape)
+        return concatenated_features
+
+
+# Create a classifier on top of the feature extractor
+class Classifier(nn.Module):
+    def __init__(self, num_classes):
+        super(Classifier, self).__init__()
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(7168, num_classes),  # Concatenated feature length from layers 2, 3, and 4
+            nn.Softmax(dim=1)  # Softmax for classification
+        )
+
+    def forward(self, features):
+        out = self.classifier(features)
+        return out
+
+feature_extractor = FeatureExtractor(discriminator).to(device)
+num_classes = 10  
+classifier = Classifier(num_classes).to(device)
+
+# Print the total number of parameters of the classifier
+total_params_classifier = sum(p.numel() for p in classifier.parameters())
+print(f"Total number of parameters in the classifier: {total_params_classifier}")
+
+# Create an optimizer and loss function for training
+learning_rate_classifier = 0.001
+optimizer_classifier = optim.Adam(classifier.parameters(), lr=learning_rate_classifier)
+criterion = nn.CrossEntropyLoss()
+
+# Training loop for the classifier (using your data loader)
+def train_classifier(classifier, train_loader, num_epochs):
+    classifier.train()
+    train_loss_history = []
+    train_accuracy_history = []
+
+    for epoch in range(num_epochs):
+        total_loss = 0.0
+        correct_predictions = 0
+        total_samples = 0
+
+        for batch in train_loader:
+            inputs, labels = batch['voxel'].unsqueeze(1).to(device), batch['label'].to(device)  # Adjust according to your dataset
+            optimizer_classifier.zero_grad()
+
+            # Forward input through the modified feature extractor
+            features = feature_extractor(inputs)
+
+            outputs = classifier(features)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer_classifier.step()
+
+            total_loss += loss.item()
+            _, predicted = torch.max(outputs, 1)
+            correct_predictions += (predicted == labels).sum().item()
+            total_samples += labels.size(0)
+
+        train_loss = total_loss / len(train_loader)
+        train_accuracy = correct_predictions / total_samples
+
+        train_loss_history.append(train_loss)
+        train_accuracy_history.append(train_accuracy)
+
+        print(f"Epoch [{epoch+1}/{num_epochs}] - Train Loss: {train_loss:.4f}, Train Accuracy: {train_accuracy:.4f}")
+
+    return train_loss_history, train_accuracy_history
+
+# You can adjust the number of epochs as needed
+num_epochs_classifier = 2
+
+# Replace train_loader with your actual data loader
+train_loader, _ = dataset.get_dataloaders(batch_size=2)
+train_loss_history, train_accuracy_history = train_classifier(classifier, train_loader, num_epochs_classifier)
+
+# Plot training loss and accuracy history
+plt.figure(figsize=(12, 4))
+plt.subplot(1, 2, 1)
+plt.plot(train_loss_history)
+plt.title("Training Loss")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+
+plt.subplot(1, 2, 2)
+plt.plot(train_accuracy_history)
+plt.title("Training Accuracy")
+plt.xlabel("Epoch")
+plt.ylabel("Accuracy")
+
+plt.show()
---
+++ train.py
@@ -0,0 +1,114 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from tqdm import tqdm
+import dataset
+from networks import Generator, Discriminator
+from torchsummary import summary
+import matplotlib.pyplot as plt
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+generator = Generator().to(device)
+discriminator = Discriminator().to(device)
+
+# Number of parameters
+# print(summary(generator, (200, 1, 1, 1)))
+# print(summary(discriminator, (1,64,64,64)))
+
+# "We set the learning rate of G to 0.0025,D to 10−5,and use a batch size of 100"
+learning_rate_G = 0.0025
+learning_rate_D = 1e-5
+batch_size = 100
+epochs = 1000
+loss = nn.BCELoss()
+
+# "We use ADAM for optimization, with β = 0.5"
+optimizer_G = optim.Adam(generator.parameters(), lr=learning_rate_G, betas=(0.5, 0.999))
+optimizer_D = optim.Adam(discriminator.parameters(), lr=learning_rate_D, betas=(0.5, 0.999))
+
+train_loader, _ = dataset.get_dataloaders(batch_size=batch_size)
+
+# Training loop
+def train():
+    D_loss = []
+    G_loss = []
+    for epoch in range(1, epochs+1):
+        tqdm_batch = tqdm(train_loader, total=len(train_loader), leave=False, dynamic_ncols=True)
+        for _, batch in enumerate(tqdm_batch):
+            real_data = batch['voxel']  # Batch of real 3D voxel samples
+            real_data = real_data.unsqueeze(1).to(device)
+            batch_size = real_data.size(0)
+
+            # Discriminator training
+            optimizer_D.zero_grad()
+            real_labels = torch.ones(batch_size, 1, 1, 1, 1, dtype=torch.float32).to(device)
+            fake_labels = torch.zeros(batch_size, 1, 1, 1, 1, dtype=torch.float32).to(device)
+
+            # Forward pass for real data
+            real_outputs = discriminator(real_data)
+
+            # Generate fake data and forward pass for fake data
+            z = torch.randn(batch_size, 200, 1, 1, 1).to(device)  # Sample random noise
+            fake_data = generator(z)
+            fake_outputs = discriminator(fake_data.detach())
+
+            # Discriminator loss
+            d_loss_real = loss(real_outputs, real_labels)
+            d_loss_fake = loss(fake_outputs, fake_labels)
+            d_loss = d_loss_real + d_loss_fake
+            d_loss.backward()
+            optimizer_D.step()
+
+            # Generator training
+            optimizer_G.zero_grad()
+            z = torch.randn(batch_size, 200, 1, 1, 1).to(device)  # Sample random noise
+            fake_data = generator(z)
+            fake_outputs = discriminator(fake_data)
+
+            # Generator loss
+            g_loss = loss(fake_outputs, real_labels)
+            g_loss.backward()
+            optimizer_G.step()
+
+            # Adaptive training strategy for the discriminator
+            # Assuming accuracy is computed based on how well D discriminates real vs. fake
+            accuracy = ((real_outputs > 0.5).float().mean() + (fake_outputs <= 0.5).float().mean()) / 2
+            if accuracy < 0.8:
+                optimizer_D.step()  # Update D again if accuracy is below 0.8
+
+            # print(torch.count_nonzero(fake_data[0] > 0.5).item())
+
+        D_loss.append(d_loss.item())
+        G_loss.append(g_loss.item())
+
+        # Print loss and accuracy for monitoring
+        tqdm_batch.set_description(f"Epoch [{epoch+1}/{epochs}] - D Loss: {d_loss.item():.4f}, G Loss: {g_loss.item():.4f}, D Accuracy: {accuracy.item():.4f}")
+        print(f"Epoch [{epoch+1}/{epochs}] - D Loss: {d_loss.item():.4f}, G Loss: {g_loss.item():.4f}")
+
+        if epoch % 100 == 0:
+            torch.save(generator.state_dict(), f"generator_ckpt_{epoch}")
+            torch.save(discriminator.state_dict(), f"discriminator_ckpt_{epoch}")
+
+    # plot losses
+    plt.figure(figsize=(10, 5))
+    plt.plot(D_loss, label="D Loss")
+    plt.plot(G_loss, label="G Loss")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.show()
+
+def generate_samples(number_samples):
+    generator.load_state_dict(torch.load(f"generator_ckpt_1000"))
+    for i in range(number_samples):
+        z = torch.randn(1, 200, 1, 1, 1).to(device)  # Sample random noise
+        fake_data = generator(z)
+        fake_data = (fake_data[0][0] > 0.5).detach().cpu().numpy()
+        ax = plt.figure().add_subplot(projection='3d')
+        ax.voxels(fake_data)
+        plt.show()
+
+#train()
+generate_samples(4)
