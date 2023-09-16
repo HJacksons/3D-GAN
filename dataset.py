cc032e025af35e1352273b312f8ce2b0f64ad8c0
HJacksons <jacksonherberts@gmail.com>
2023-09-17 00:59:46 +0200

    code refactor

--- dataset.py
+++ dataset.py
@@ -6,7 +6,7 @@ import torch
 
 class VoxelDataset(Dataset):
     def __init__(self, data, labels):
-        self.data = torch.tensor(data, dtype=torch.float32)  
+        self.data = torch.tensor(data, dtype=torch.float32)
         self.labels = torch.tensor(labels, dtype=torch.int64)
 
     def __len__(self):
@@ -23,11 +23,11 @@ class VoxelDataset(Dataset):
 
 def get_dataloaders(batch_size):
     data = np.load("modelnet10.npz", allow_pickle=True)
-    train_voxel = data["train_voxel"] # Training 3D voxel samples
-    test_voxel = data["test_voxel"] # Test 3D voxel samples
-    train_labels = data["train_labels"] # Training labels (integers from 0 to 9)
-    test_labels = data["test_labels"] # Test labels (integers from 0 to 9)
-    class_map = data["class_map"] # Dictionary mapping the labels to their class names.
+    train_voxel = data["train_voxel"]  # Training 3D voxel samples
+    test_voxel = data["test_voxel"]  # Test 3D voxel samples
+    train_labels = data["train_labels"]  # Training labels (integers from 0 to 9)
+    test_labels = data["test_labels"]  # Test labels (integers from 0 to 9)
+    class_map = data["class_map"]  # Dictionary mapping the labels to their class names.
 
     train_dataset = VoxelDataset(train_voxel, train_labels)
     test_dataset = VoxelDataset(test_voxel, test_labels)
